665dd1605be946f10320df6e6088cb6d726366b1
Melec <duhalgouetm@yahoo.com>
2020-10-27 19:42:38 +0100

    handleInputChange done

--- src/components/FindCrewmate.jsx
+++ src/components/FindCrewmate.jsx
@@ -10,21 +10,50 @@ const FindCrewmate = () => {
 
     const [fieldValues, setFieldValues] = useState(initialFieldValues);
 
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setFieldValues({
+            ...fieldValues,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log('recreation')
+        console.log(fieldValues)
+    }
+
     return (
         //maybe add fonticon awesome...
-        <form autoComplete="off">
-                        <div className="form-row">
-            <div className="form-group input-group col-md-6">
-                    <input className="form-control" placeholder="name" name="name" values={fieldValues.name}></input>
-            </div>
-            <div className="form-group input-group col-md-6">
-                    <input className="form-control" placeholder="surname" name="surmane" value={fieldValues.surname}></input>
-            </div>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+            <div className="form-row">
+                <div className="form-group input-group col-md-6">
+                    <input 
+                    className="form-control" 
+                    placeholder="name" 
+                    name="name" 
+                    values={fieldValues.name} 
+                    onChange={handleInputChange}></input>
+                </div>
+                <div className="form-group input-group col-md-6">
+                    <input 
+                    className="form-control" 
+                    placeholder="surname" 
+                    name="surname" 
+                    value={fieldValues.surname}
+                    onChange={handleInputChange}></input>
+                </div>
             </div>
             <div className="form-group input-group">
-
-                    <input className="form-control" placeholder="speciality" name="speciality" values={fieldValues.speciality}></input>
-
+                <input 
+                className="form-control" 
+                placeholder="speciality" name="speciality" 
+                values={fieldValues.speciality}
+                onChange={handleInputChange}></input>
+            </div>
+            <div className="form-group">
+                <input type="submit" value="SaveForm" className="btn btn-secondary btn-block"/>
             </div>
         </form> 
     );
